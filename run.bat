bcb189a4dd597a42ec0b48136303190f0b868992
Martin Pávek <pavek_martin@seznam.cz>
2026-05-22 12:57:22 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -6,7 +6,6 @@ echo napr. wiki_1.txt
 set /p volba="nazev souboru : "
 echo %volba%
 
-REM powershell -file ffmpeg_vyrezi_videa_spojeni_vyrezu_3.ps1 %volba%
-ffmpeg_vyrezi_videa_spojeni_vyrezu_3.exe %volba%
+powershell -file ffmpeg_vyrezi_videa_spojeni_vyrezu_3.ps1 %volba%
 pause
 
